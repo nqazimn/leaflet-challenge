855abffc507fba9ccf7c615567dadf36728f12fc
nqazimn <nqazimn@gmail.com>
2020-04-17 16:29:30 -0500

    added highlight on hover capability on us map

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,19 +3,65 @@
  *  Created On : Fri Apr 17 2020
  *  File : logic.js
  *******************************************/
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'map'
+
+var states_outlines = "./static/data/us_states.geojson";
+async function getData(url) {
+    let response = await fetch(url);
+    let json = await response.json();
+    return json;
+}
+
+var maxBounds = L.latLngBounds(
+    L.latLng(5.499550, -167.276413), //Southwest
+    L.latLng(83.162102, -52.233040)  //Northeast
+);
+
 var myMap = L.map("map", {
-    center: [44.99, -93.26],
-    zoom: 12
+    center: [44.828175, -102.5795],
+    zoom: 4
 });
 
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
+
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.light",
-    accessToken: API_KEY
+    accessToken: API_KEY,
 }).addTo(myMap);
+
+// Make US map prominent
+var defaultStyle = {
+    "color": "#FFFFFF",
+    "weight": 1.25,
+    "opacity": 1,
+    "stroke": true,
+    "fill": true,
+    "fillOpacity": 1,
+    "fillColor": "#008080"
+};
+
+var highlightStyle = {
+    "color": '#FFFFFF',
+    "weight": 2.5,
+    opacity: 1,
+    "stroke": true,
+    "fill": true,
+    "fillOpacity": 1,
+    fillOpacity: 1,
+    fillColor: '#2262CC'
+};
+
+d3.json(states_outlines, function (data) {
+    L.geoJson(data, {
+        style: defaultStyle,
+        onEachFeature: function (feature, Layer) {
+            Layer.on('mouseover', function () {
+                this.setStyle(highlightStyle)
+            });
+            Layer.on('mouseout', function () {
+                this.setStyle(defaultStyle)
+            });
+        }
+    }).addTo(myMap);
+});
+
