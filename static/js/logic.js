b89170f6f2afc881050c28aa74effa17fc64f96f
nqazimn <nqazimn@gmail.com>
2020-04-25 07:55:15 -0500

    added code to size and colour markers. deleted json

--- static/js/logic.js
+++ static/js/logic.js
@@ -19,10 +19,21 @@ var maxBounds = L.latLngBounds(
 );
 
 var myMap = L.map("map", {
-    center: [0, 0],
+    center: [30, 0],
     zoom: 3
 });
 
+var maxRadius = 100000;
+var minRadius = 10000;
+
+// Normalizes the given magnitude between lo <-> hi for 
+// better visualization of markers
+function getMarkerRadius(val, minVal, maxVal, lo, hi) {
+    // Normalize magnitude between 0 to 1
+    var normalizedValue = ((val - minVal) / (maxVal - minVal));
+
+    return (lo + normalizedValue * (hi - lo));
+}
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -51,8 +62,9 @@ var highlightStyle = {
     "fillColor": '#008080'
 };
 
+// Adding country boundries and enabling hover feature
 d3.json(countryOutlines, function (data) {
-    console.log(data);
+    // console.log(data);
     L.geoJson(data, {
 
         style: defaultStyle,
@@ -68,36 +80,97 @@ d3.json(countryOutlines, function (data) {
     }).addTo(myMap);
 });
 
+// Adding Equator Line
+L.polyline([[0, -360], [0, 360]], {
+    color: "teal",
+    weight: 2,
+    dashArray: '10, 10'
+}).addTo(myMap);
+
+// Borrowing colorScale function from Project 2
+// colorScale function
+function getColor(d) {
+    return d > 5 ? 'rgb(240,107,107)' :
+        d > 4 ? 'rgb(240,167,107)' :
+            d > 3 ? 'rgb(243,186,77)' :
+                d > 2 ? 'rgb(243,219,77)' :
+                    d > 1 ? 'rgb(225,243,77)' :
+                        'rgb(183,243,77)';
+}
+// Adding markers for earthquake locations
 d3.json(earthquake_url, function (eqData) {
-    console.log(eqData.features);
-    console.log(eqData.features[0]);
-    // console.log(eqData.features.length);
+
+    var arrayOfMagnitudes = [];
+    Object.entries(eqData.features).forEach(function ([keys, values]) {
+        arrayOfMagnitudes.push(values.properties.mag)
+    });
+    var minimumMagnitude = Math.min.apply(Math, arrayOfMagnitudes);
+    var maximumMagnitude = Math.max.apply(Math, arrayOfMagnitudes);
+
+    // var lowest = Math.min.apply(null, keys.map(function(x) { return obj[x]} ));
 
 
     for (var i = 0; i < eqData.features.length; i++) {
         var eqCoordinates = eqData.features[i].geometry.coordinates.slice(0, 2);
         var eqLatLon = [eqCoordinates[1], eqCoordinates[0]]
         var eqMagnitude = (eqData.features[i].properties.mag);
-        // console.log(eqMagnitude * 20000);
+        var markerRadius = getMarkerRadius(eqMagnitude,
+            minimumMagnitude,
+            maximumMagnitude,
+            0,
+            4);
+        // console.log(2 ** markerRadius);
+
 
         if (Math.sign(eqMagnitude) > 0) {
             L.circle(eqLatLon, {
-                fillOpacity: 0.5,
-                color: "None",
-                fillColor: "orange",
-                radius: eqMagnitude * 20000
+                fillOpacity: 0.75,
+                color: getColor(eqMagnitude),//getColor(markerRadius),
+                // fillColor: "orange",
+                fillColor: getColor(eqMagnitude),
+                radius: (2 ** markerRadius) * minRadius
             }).bindPopup("<h1>" + "TEST" + "</h1> <hr> <h3>Population: " + "Magnitude?" + "</h3>").addTo(myMap);
         }
         if (Math.sign(eqMagnitude) < 0) {
+            // console.log(eqMagnitude);
             L.circle(eqLatLon, {
-                fillOpacity: 0.5,
-                color: "None",
-                fillColor: "red",
-                radius: Math.abs(eqMagnitude) * 20000
+                fillOpacity: 1,
+                color: "black",//getColor(markerRadius),
+                // fillColor: "orange",
+                fillColor: "black",//getColor(markerRadius),
+                radius: (2 ** markerRadius) * minRadius
             }).bindPopup("<h1>" + "TEST" + "</h1> <hr> <h3>Population: " + "Magnitude?" + "</h3>").addTo(myMap);
         }
-
 
     }
 
 })
+
+
+// Set up the legend
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function () {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = geojson.options.limits;
+    var colors = geojson.options.colors;
+    var labels = [];
+
+    // Add min & max
+    var legendInfo = "<h1>Median Income</h1>" +
+        "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+        "</div>";
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function (limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+};
+
+// Adding legend to the map
+legend.addTo(myMap);
